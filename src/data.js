e71facf7cda5cb093ff8e940368b6dab6d77227c
NikolayTsvetkov <nikolay.val.tsvetkov@gmail.com>
2023-09-20 11:38:32 +0300

    Implementing useReducer, questions, global state

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    question: "Which one is among the seven wonders of the world? ",
+    incorrectAnswers: ["The Eifel Tower", "Mona Liza", "The tample Angkor Wat"],
+    correctAnswer: "The Great Pyramid of Giza",
+  },
+
+  {
+    question: "How many spectators could the Roman Colosseum hold? ",
+    incorrectAnswers: ["30 000", "50 000", "70 000"],
+    correctAnswer: "80 000",
+  },
+  {
+    question: "Which HTML tag is used to define an internal style sheet?",
+    incorrectAnswers: ["<script>", "<headStyle>", "<css>"],
+    correctAnswer: "<style>",
+  },
+  {
+    question: "Which country is the birthplace of democracy? ",
+    incorrectAnswers: ["Italy", "Great Britain", "The USA"],
+    correctAnswer: "Greece",
+  },
+];
+
+export default data;
